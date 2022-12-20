fc7b215a8cbcc94c3cf47db8f6550e7961434d58
radik-cs <84479742+radik-cs@users.noreply.github.com>
2022-12-20 15:39:01 -0500

    First commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
+@app.route('/about')
+def about():
+    return "About"
+
+
+ 
+    if __name__ =="__main__":
+        app.run(debuf=True)
