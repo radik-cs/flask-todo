e75bcfc5541dac5fa2e94e124071152b4794f1c9
radik-cs <84479742+radik-cs@users.noreply.github.com>
2022-12-22 15:18:32 -0500

    Jinja

--- app.py
+++ app.py
@@ -3,15 +3,26 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
 
 
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+
+    # show all todos
+    todo_list = Todo.query.all()
+    print(todo_list)
+
+    return render_template('base.html', todo_list=todo_list)
 
 
 @app.route('/about')
@@ -20,6 +31,13 @@ def about():
 
 
  
-    if __name__ =="__main__":
+    if __name__ == "__main__":
         db.create_all()
-        app.run(debuf=True)
+
+        new_todo = Todo(title="todo 1", complete=False)
+        db.session.add(new_todo)
+        db.session.commit()
+
+
+
+        app.run(debug=True)
